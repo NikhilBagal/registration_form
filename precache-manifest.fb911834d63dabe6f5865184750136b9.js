24faf4ce04f45f50873f79c95ab0eb9097f350b5
Nikhil Bagal <nbagal02@gmail.com>
2020-07-31 11:32:56 +0530

    Updates

---
+++ precache-manifest.fb911834d63dabe6f5865184750136b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f3bf16d48fe45a760a6246082bf310",
+    "url": "/registration_form/index.html"
+  },
+  {
+    "revision": "1d0c342240e55991dcbc",
+    "url": "/registration_form/static/css/main.58b26ef7.chunk.css"
+  },
+  {
+    "revision": "ca73770fc604accea8fe",
+    "url": "/registration_form/static/js/2.c3a14667.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registration_form/static/js/2.c3a14667.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0c342240e55991dcbc",
+    "url": "/registration_form/static/js/main.c5adce2b.chunk.js"
+  },
+  {
+    "revision": "0e168fe0560b2bfb4065",
+    "url": "/registration_form/static/js/runtime-main.452da4b5.js"
+  }
+]);
